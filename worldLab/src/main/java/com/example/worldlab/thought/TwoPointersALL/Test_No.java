e487d5880fbe6940ee94ff0fbb0fe489f5864165
朱强龙 <zhuqianglong@bigo.sg>
2025-11-06 17:07:00 +0800

    feat: 提交树的高度以及对象池实现的相关代码

--- worldLab/src/main/java/com/example/worldlab/thought/TwoPointersALL/Test_No.java
+++ worldLab/src/main/java/com/example/worldlab/thought/TwoPointersALL/Test_No.java
@@ -11,6 +11,48 @@ package com.example.myworld.aleetcode;
 class Test_No {
 
     public static void main(String[] args) {
+        ObjectPool<StringBulder> pool= new ObjectPool<StringBulder>(
+                ()->new StringBuilder(), (sb)-> {sb.clear()},  100
+        );
 
+        StringBuilder sb = pool.getObject();
+        pool.returnObject(sb);
+
+    }
+
+    public class ObjectPool<T>{
+        private Queue queue= new ConcurrentLinkdQueue();
+        prirvate int maxSize;
+        private Supplier<T> supplier;
+        private Consumer<T> consumer;
+
+        public ObjectPool(Supplier<T> supplier,
+        Consumer<T> consumer, int maxSize){
+            this.supplier= supplier;
+            this.consumer= consumer;
+            this.maxSize=maxSize;
+        }
+
+        public T getObject(){
+            T object =null;
+            if(queue.size>0){
+                object = queue.poll();
+            }else{
+                object = supplier.get();
+            }
+            return object;
+        }
+        public void returnObject(T object){
+            if(object!=null && queue.size()< maxSize){
+                if(consumer!=null){
+                    consumer.accept(object)
+                }
+                queue.offer(object)
+            }
+        }
+
+        public int getCount(){
+            return queue.size();
+        }
     }
 }
